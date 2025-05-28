bf7a7df86121d719829ea081ab7eae1dda6d1f58
Finnbur <93823@roc-teraa.nl>
2025-05-28 21:15:41 +0200

    added the dogDeck

---
+++ items/decks.lua
@@ -0,0 +1,48 @@
+SMODS.Atlas{
+    key = 'dogdeck',
+    path = 'dogdeck.png',
+    px = 71,
+    py = 95,
+}
+
+SMODS.Back({
+    key = "dog_deck",
+    loc_txt = {
+        name = "Dog",
+        text={
+        "Start with 5 dog jokers",
+        },
+    },
+	
+	config = { hands = 0, discards = 0, joker = 'dog'},
+	pos = { x = 0, y = 0 },
+	order = 1,
+	atlas = "dogdeck",
+    unlocked = true,
+
+	apply = function(self)
+        G.E_MANAGER:add_event(Event({
+			func = function()
+				if G.consumeables then
+                        local card = create_card("Joker", G.jokers, "j_finnmod_dog", "j_finnmod_dog", "j_finnmod_dog", "j_finnmod_dog", "j_finnmod_dog", "finnmod_deck")
+                        card:add_to_deck()
+                        --card:start_materialize()
+                        G.jokers:emplace(card)
+                        G.jokers:emplace(card)
+                        G.jokers:emplace(card)
+                        G.jokers:emplace(card)
+                        G.jokers:emplace(card)
+                  end
+                  return true
+			end,
+		}))
+	end,
+
+	check_for_unlock = function(self, args)
+		if args.type == "win_deck" then
+            unlock_card(self)
+        else
+			unlock_card(self)
+		end
+	end,
+})
