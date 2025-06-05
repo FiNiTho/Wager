f39823d5bf0e66cef8a7ae4d13ba627c60082bbd
FiNiTho <burghaus.finn@gmail.com>
2025-06-05 23:51:04 +0200

    updated the dog deck

--- items/decks.lua
+++ items/decks.lua
@@ -10,10 +10,12 @@ SMODS.Back({
     loc_txt = {
         name = "Dog",
         text={
-        "Start with {C:attention}5{}",
-        "dog jokers"
+        "Start with {C:attention}5{} dog jokers",
+        "create another one",
+        "when boss blind is defeated",
+        "{C:inactive}(must have room){}"
         },
-        locked = {
+        unlock = {
         "Unlock by exploding the",
         "{C:attention}Dog{} Joker."
         },
@@ -28,19 +30,42 @@ SMODS.Back({
 
 	apply = function(self)
         G.E_MANAGER:add_event(Event({
-			func = function()
-				if G.consumeables then
-                        for i = 1, 5 do
-                            local card = create_card("Joker", G.jokers, nil, nil, nil, nil, "j_finnmod_dog", "finnmod_deck")
-                            -- card:set_edition("e_negative", true)
-                            card:add_to_deck()
-                            G.jokers:emplace(card)
-                        end
-                  end
-                  return true
-			end,
-		}))
-	end,
+            func = function()
+                -- Add 5 jokers at apply-time if consumeables exist
+                if G.consumeables then
+                    for i = 1, 5 do
+                        local card = create_card("Joker", G.jokers, nil, nil, nil, nil, "j_finnmod_dog", "finnmod_deck")
+                        -- card:set_edition("e_negative", true)
+                        card:add_to_deck()
+                        G.jokers:emplace(card)
+                    end
+                end
+                return true
+            end
+        }))
+    end,
+
+    calculate = function(self, back, context)
+        if context.end_of_round
+            and G.GAME.blind:get_type() == 'Boss'
+            and not G.GAME.round_resets.dog_joker_given
+        then
+            G.GAME.round_resets.dog_joker_given = true
+
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    if #G.jokers.cards < G.jokers.config.card_limit then
+                        local card = create_card("Joker", G.jokers, nil, nil, nil, nil, "j_finnmod_dog", "finnmod_deck")
+                        card:add_to_deck()
+                        G.jokers:emplace(card)
+                    end
+                    G.GAME.round_resets.dog_joker_given = false
+                    return true
+                end
+            }))
+        end
+    end,
+
 
 	check_for_unlock = function(self, args)
         if G.GAME.pool_flags and G.GAME.pool_flags.dog_exploded then
