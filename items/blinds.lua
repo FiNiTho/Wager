2c204493820814b929dca520b425ea1782b80415
FiNiTho <burghaus.finn@gmail.com>
2025-11-15 00:21:19 +0100

    1.2 update wrapped up

--- items/blinds.lua
+++ items/blinds.lua
@@ -1,68 +1,68 @@
--- TODO: make this boss work that it chooses leftmost joker or rightmost and after 4 antes or something does both
--- left right boss
-SMODS.Blind {
-    key = "left_right",
-    dollars = 5,
-    mult = 2,
-    pos = { x = 0, y = 16 },
-    boss = { min = 2 },
-    boss_colour = HEX("7e6752"),
-     calculate = function(self, blind, context)
-        if not blind.disabled then
-            if context.debuff_card and context.debuff_card.area == G.jokers then
-                if context.debuff_card.ability.crimson_heart_chosen then
-                    return {
-                        debuff = true
-                    }
-                end
-            end
-            if context.press_play and G.jokers.cards[1] then
-                blind.triggered = true -- This won't trigger Matador in this context due to a Vanilla bug (a workaround is setting it in context.debuff_hand)
-                blind.prepped = true
-            end
-            if context.hand_drawn then
-                if blind.prepped and G.jokers.cards[1] then
-                    local prev_chosen_set = {}
-                    local fallback_jokers = {}
-                    local jokers = {}
-                    for i = 1, #G.jokers.cards do
-                        if G.jokers.cards[i].ability.crimson_heart_chosen then
-                            prev_chosen_set[G.jokers.cards[i]] = true
-                            G.jokers.cards[i].ability.crimson_heart_chosen = nil
-                            if G.jokers.cards[i].debuff then SMODS.recalc_debuff(G.jokers.cards[i]) end
-                        end
-                    end
-                    for i = 1, #G.jokers.cards do
-                        if not G.jokers.cards[i].debuff then
-                            if not prev_chosen_set[G.jokers.cards[i]] then
-                                jokers[#jokers + 1] = G.jokers.cards[i]
-                            end
-                            table.insert(fallback_jokers, G.jokers.cards[i])
-                        end
-                    end
-                    if #jokers == 0 then jokers = fallback_jokers end
-                    local _card = pseudorandom_element(jokers, 'vremade_crimson_heart')
-                    if _card then
-                        _card.ability.crimson_heart_chosen = true
-                        SMODS.recalc_debuff(_card)
-                        _card:juice_up()
-                        blind:wiggle()
-                    end
-                end
-            end
-        end
-        if context.hand_drawn then
-            blind.prepped = nil
-        end
-    end,
-    disable = function(self)
-        for _, joker in ipairs(G.jokers.cards) do
-            joker.ability.crimson_heart_chosen = nil
-        end
-    end,
-    defeat = function(self)
-        for _, joker in ipairs(G.jokers.cards) do
-            joker.ability.crimson_heart_chosen = nil
-        end
-    end
-}
+-- -- TODO: make this boss work that it chooses leftmost joker or rightmost and after 4 antes or something does both
+-- -- left right boss
+-- SMODS.Blind {
+--     key = "left_right",
+--     dollars = 5,
+--     mult = 2,
+--     pos = { x = 0, y = 16 },
+--     boss = { min = 2 },
+--     boss_colour = HEX("7e6752"),
+--      calculate = function(self, blind, context)
+--         if not blind.disabled then
+--             if context.debuff_card and context.debuff_card.area == G.jokers then
+--                 if context.debuff_card.ability.crimson_heart_chosen then
+--                     return {
+--                         debuff = true
+--                     }
+--                 end
+--             end
+--             if context.press_play and G.jokers.cards[1] then
+--                 blind.triggered = true -- This won't trigger Matador in this context due to a Vanilla bug (a workaround is setting it in context.debuff_hand)
+--                 blind.prepped = true
+--             end
+--             if context.hand_drawn then
+--                 if blind.prepped and G.jokers.cards[1] then
+--                     local prev_chosen_set = {}
+--                     local fallback_jokers = {}
+--                     local jokers = {}
+--                     for i = 1, #G.jokers.cards do
+--                         if G.jokers.cards[i].ability.crimson_heart_chosen then
+--                             prev_chosen_set[G.jokers.cards[i]] = true
+--                             G.jokers.cards[i].ability.crimson_heart_chosen = nil
+--                             if G.jokers.cards[i].debuff then SMODS.recalc_debuff(G.jokers.cards[i]) end
+--                         end
+--                     end
+--                     for i = 1, #G.jokers.cards do
+--                         if not G.jokers.cards[i].debuff then
+--                             if not prev_chosen_set[G.jokers.cards[i]] then
+--                                 jokers[#jokers + 1] = G.jokers.cards[i]
+--                             end
+--                             table.insert(fallback_jokers, G.jokers.cards[i])
+--                         end
+--                     end
+--                     if #jokers == 0 then jokers = fallback_jokers end
+--                     local _card = pseudorandom_element(jokers, 'vremade_crimson_heart')
+--                     if _card then
+--                         _card.ability.crimson_heart_chosen = true
+--                         SMODS.recalc_debuff(_card)
+--                         _card:juice_up()
+--                         blind:wiggle()
+--                     end
+--                 end
+--             end
+--         end
+--         if context.hand_drawn then
+--             blind.prepped = nil
+--         end
+--     end,
+--     disable = function(self)
+--         for _, joker in ipairs(G.jokers.cards) do
+--             joker.ability.crimson_heart_chosen = nil
+--         end
+--     end,
+--     defeat = function(self)
+--         for _, joker in ipairs(G.jokers.cards) do
+--             joker.ability.crimson_heart_chosen = nil
+--         end
+--     end
+-- }
